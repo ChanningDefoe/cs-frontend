797f3b3ffb7e46cf83dc928d9b446a204eb105ce
Channing Defoe <channingdefoe@delta.edu>
2021-11-21 21:18:41 -0500

    add some auth stuff

---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">CS Frontend</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <template v-if="authenticated">
+            <li class="nav-item">
+              <a href="/inventory" class="nav-link">Inventory</a>
+            </li>
+            <li class="nav-item">
+              <a href="/products" class="nav-link">Products</a>
+            </li>
+          </template>
+        </ul>
+        <template v-if="authenticated">
+          <form class="form-inline my-2 my-lg-0" @submit="logout">
+            <button class="btn btn-outline my-2 my-sm-0" type="submit">
+              Log Out
+            </button>
+          </form>
+        </template>
+      </div>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    authenticated() {
+      return this.$auth.loggedIn
+    },
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+      this.$router.push('/')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,41 @@
   <div>
     <div class="container mt-5">
       <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-header">Login</div>
-            <div class="card-body">
-              <form @submit.prevent="login">
-                <div class="form-group">
-                  <label>Username</label>
-                  <input
-                    class="form-control"
-                    type="text"
-                    v-model="loginForm.email"
-                  />
-                </div>
-                <div class="form-group">
-                  <label>Password</label>
-                  <input
-                    class="form-control"
-                    type="text"
-                    v-model="loginForm.password"
-                  />
-                </div>
-                <div>
-                  <button class="btn btn-primary" type="submit">Submit</button>
-                </div>
-              </form>
+        <template v-if="!authenticated">
+          <div class="col">
+            <div class="card">
+              <div class="card-header">Login</div>
+              <div class="card-body">
+                <form @submit.prevent="login">
+                  <div class="form-group">
+                    <label>Username</label>
+                    <input
+                      class="form-control"
+                      type="text"
+                      v-model="loginForm.email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label>Password</label>
+                    <input
+                      class="form-control"
+                      type="text"
+                      v-model="loginForm.password"
+                    />
+                  </div>
+                  <div>
+                    <button class="btn btn-primary" type="submit">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </template>
+        <template v-else>
+          <h1>Welcome to the Homepage</h1>
+        </template>
       </div>
     </div>
   </div>
@@ -44,6 +51,11 @@ export default {
         password: '',
       },
     }
+  },
+  computed: {
+    authenticated() {
+      return this.$auth.loggedIn
+    },
   },
   methods: {
     async login() {
